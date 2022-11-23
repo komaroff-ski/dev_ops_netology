5663d658f2b6d21c7698067483f80b6bbfc23fee
komaroff-ski <moskitou13@gmail.com>
2022-11-23 18:15:59 +0300

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   homeworks/mnt/ansible-03/terraform/main.tf

--- homeworks/mnt/ansible-03/terraform/main.tf
+++ homeworks/mnt/ansible-03/terraform/main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "yandex" {
-  token     = "AQAEA7qhzo99AATuwfR1EuruNUlDsu6XzQmOCfE"
+  token     = ""
   cloud_id  = "b1g0drs0h158qiurq3qa"
   folder_id = "b1g3i641nh19u19bfr2q"
   zone      = "ru-central1-a"
